64264c02ba79623f2ef28e10a412054342f50177
Ilia Pasechnikov <ilia.pasechnikov@gmail.com>
2023-03-28 01:18:19 +0900

    Add door and screen interactive devices

---
+++ Assets/Scripts/DoorOpenDevice.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorOpenDevice : MonoBehaviour
+{
+    // Amount to offset the position by when the door opens
+    [SerializeField] Vector3 dPos;
+
+    // Keep track of the open state of the door
+    private bool open;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Operate()
+    {
+        if (open)
+        {
+            var pos = transform.position - dPos;
+            transform.position = pos;
+        }
+        else
+        {
+            var pos = transform.position + dPos;
+            transform.position = pos;
+
+        }
+        open = !open;
+    }
+}
