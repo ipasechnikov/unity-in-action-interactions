7d0206a064d0c84c10a649c76b5dda44a0a2d18e
Ilia Pasechnikov <ilia.pasechnikov@gmail.com>
2023-03-31 17:19:40 +0900

    Add collectible items to the level

---
+++ Assets/Scripts/CollectibleItem.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectibleItem : MonoBehaviour
+{
+    [SerializeField] string itemName;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log($"Item collected: {itemName}");
+        Destroy(gameObject);
+    }
+}
